b473774508e71f9b44b0f6757138131cdb5b4ccd
Ech0ke <aidas.zygas@gmail.com>
2024-02-26 16:28:06 +0200

    Automate math problem solving with new tab opening case

---
+++ solve_maths/form_redirect_test.py
@@ -0,0 +1,64 @@
+import re
+import math
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+PATH = "C:\Drivers\chromedriver\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+EQUATION_PATTERN = r'\bln\(abs\(12\*sin\(x\)\)\)'
+
+
+def reformat_equation(math_equation, x_value):
+    math_equation = math_equation.replace("x", x_value)
+    math_equation = math_equation.replace("ln", "math.log")
+    math_equation = math_equation.replace("sin", "math.sin")
+    return math_equation
+
+
+# Load the website
+driver.get("http://suninjuly.github.io/redirect_accept.html")
+
+# Find accept button
+accept_button = driver.find_element_by_css_selector("button[type='submit']")
+
+# Click accept button
+accept_button.click()
+
+# Wait for redirected page to load
+WebDriverWait(driver, 10).until(EC.url_changes)
+
+# Switch to new tab
+driver.switch_to.window(driver.window_handles[1])
+
+# Get math equation sentence
+math_equation_sentence = driver.find_element_by_css_selector(
+    "div.form-group > label > span:first-child").text
+
+# Extract only the equation
+math_equation = re.search(EQUATION_PATTERN, math_equation_sentence).group()
+
+# Get value of x needed to solve the equation
+x_value = driver.find_element_by_css_selector("#input_value").text
+
+# Perform some equation parts reformatting in order to pass it to eval later
+reformatted_math_equation = reformat_equation(math_equation, x_value)
+
+result = eval(reformatted_math_equation)
+
+# Get equation answer field
+result_input_field = driver.find_element_by_css_selector("#answer")
+
+# Type answer into the field as string
+result_input_field.send_keys(str(result))
+
+# Submit answer
+submit_button = driver.find_element_by_css_selector("button[type='submit']")
+submit_button.click()
+
+# Print success message
+alert = driver.switch_to.alert
+print(alert.text)
+
+driver.quit()
